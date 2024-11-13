9992c69b3a85553109df9f0aac08e5da00fa5680
Polly <1327767601@qq.com>
2024-11-13 16:40:43 +0800

    刚发的施工队

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
 export default function Page() {
   return (
-    <ul>
-      <li>hahahahaha，嘻嘻,测试docker构建时使用缓存的情况,xx</li>
-    </ul>
+    <>
+      <ul>
+        <li>hahahahaha，嘻嘻,测试docker构建时使用缓存的情况,xx</li>
+      </ul>
+      <button>加了点东西</button>
+    </>
   );
 }
